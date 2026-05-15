f5ffd9d409dddec9b7ca424660852a622716807d
ismayil37 <ismayiltehmezov050@gmail.com>
2026-05-15 21:04:17 +0400

    Update Program.cs

--- EmployeePerformanceSystem/Program.cs
+++ EmployeePerformanceSystem/Program.cs
@@ -4,12 +4,12 @@ using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Servisləri əlavə et
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// CORS icazəsi
+
 builder.Services.AddCors(options => {
     options.AddPolicy("AllowAll",
         policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
@@ -17,10 +17,9 @@ builder.Services.AddCors(options => {
 
 var app = builder.Build();
 
-// --- STATİK FAYLLAR ÜÇÜN ƏLAVƏLƏR ---
-// Bu sətir brauzer açılan kimi avtomatik index.html faylını axtarır
+
 app.UseDefaultFiles();
-// Bu sətir wwwroot qovluğundakı faylları (html, js, css) xidmətə verir
+
 app.UseStaticFiles();
 // ------------------------------------
 
