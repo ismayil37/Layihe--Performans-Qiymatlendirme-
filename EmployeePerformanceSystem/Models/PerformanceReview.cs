530ef7237f940abeb6eb731f694705db6e70d120
ismayil37 <ismayiltehmezov050@gmail.com>
2026-05-15 21:00:31 +0400

    Update PerformanceReview.cs

--- EmployeePerformanceSystem/Models/PerformanceReview.cs
+++ EmployeePerformanceSystem/Models/PerformanceReview.cs
@@ -4,10 +4,10 @@
     {
         public int Id { get; set; }
         public int EmployeeId { get; set; }
-        public double TaskCompletionScore { get; set; } // 0-100 arası
-        public double QualityScore { get; set; } // Menecer rəyi (1-10 arası)
-        public double AttendanceScore { get; set; } // Davamiyyət (0-100 arası)
-        public double FinalPerformanceScore { get; set; } // Düsturdan çıxan yekun bal
+        public double TaskCompletionScore { get; set; } 
+        public double QualityScore { get; set; } 
+        public double AttendanceScore { get; set; } 
+        public double FinalPerformanceScore { get; set; } 
         public DateTime ReviewDate { get; set; }
     }
 }
