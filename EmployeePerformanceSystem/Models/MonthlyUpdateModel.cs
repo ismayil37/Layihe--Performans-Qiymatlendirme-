fb953ee2b6a61359db94c391f35f779d8982ddcb
ismayil37 <ismayiltehmezov050@gmail.com>
2026-05-15 21:00:09 +0400

    Update MonthlyUpdateModel.cs

--- EmployeePerformanceSystem/Models/MonthlyUpdateModel.cs
+++ EmployeePerformanceSystem/Models/MonthlyUpdateModel.cs
@@ -2,7 +2,7 @@
 {
     public class MonthlyUpdateModel
     {
-        public int MonthIndex { get; set; } // 0=Yan, 1=Fev, ... 4=May
+        public int MonthIndex { get; set; } 
         public int Attendance { get; set; }
         public double Performance { get; set; }
     }
