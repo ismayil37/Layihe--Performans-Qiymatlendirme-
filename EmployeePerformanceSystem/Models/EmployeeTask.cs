6c5b4eb809a06f860eb2c56d2766cfdb65bf1f28
ismayil37 <ismayiltehmezov050@gmail.com>
2026-05-15 20:59:54 +0400

    Update EmployeeTask.cs

--- EmployeePerformanceSystem/Models/EmployeeTask.cs
+++ EmployeePerformanceSystem/Models/EmployeeTask.cs
@@ -3,10 +3,10 @@
     public class EmployeeTask
     {
         public int Id { get; set; }
-        public int EmployeeId { get; set; } // Hansı işçiyə aiddir?
+        public int EmployeeId { get; set; } 
         public string Title { get; set; }
-        public DateTime DueDate { get; set; } // Bitməli olduğu vaxt
-        public DateTime? CompletedDate { get; set; } // Faktiki bitmə vaxtı
-        public bool IsCompleted { get; set; } // Tamamlanıb?
+        public DateTime DueDate { get; set; } 
+        public DateTime? CompletedDate { get; set; }
+        public bool IsCompleted { get; set; }
     }
 }
